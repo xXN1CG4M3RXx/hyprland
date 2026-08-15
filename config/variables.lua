0bdf95a0ed7269ade9f3b23f259520bc17fedcc0
Nico Jans <contenico261@gmail.com>
2026-08-15 13:31:19 +0200

    variables: update monitor variables

--- config/variables.lua
+++ config/variables.lua
@@ -7,10 +7,11 @@ EDITOR       = "gnome-text-editor --new-window"
 CALCULATOR   = "gnome-calculator"
 
 -- Monitors
-MONITOR1 = ""
-MONITOR2 = ""
+MONITOR1 = "HDMI-A-1"
+MONITOR2 = "HDMI-A-2"
 MONITOR3 = ""
 PRIMARY_MONITOR = MONITOR1
+SECONDARY_MONITOR = MONITOR2
 
 -- Workspaces
 NUM_WPM = 3 -- Number of workspaces per monitor (Max 10)
