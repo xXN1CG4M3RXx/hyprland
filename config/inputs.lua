7adea9288f11623679a21bd03d3c8efdaca2ae7e
Nico Jans <contenico261@gmail.com>
2026-08-16 21:09:41 +0200

    input: fix scrolling directoina and sensitivity for mouse

--- config/inputs.lua
+++ config/inputs.lua
@@ -4,8 +4,8 @@ hl.config({
     input = {
         -- sensitivity = -0.25,
         accel_profile = "flat",
-	natural_scroll = true,
-	scroll_factor = 0.3, 
+	natural_scroll = false,
+	scroll_factor = 1, 
     },
     -- Uncomment the section below to enable software cursors; this can help with cursor display or behavior issues
     -- cursor = {
